7a9d53c8821150bd72b385e11ef4563e5cf436d8
Soumadip Das <soumadip@zohomail.in>
2026-02-19 19:05:53 +0530

    fix(ci): Build from source in AUR as guidelines

--- scripts/version.sh
+++ scripts/version.sh
@@ -2,9 +2,9 @@
 
 export MODULE=$(grep "^module" go.mod | awk '{print $2}')
 export LATEST_TAG=$(git describe --tags --abbrev=0 2>/dev/null || echo "v0.0.0")
-export VERSION=$(git describe --tags --abbrev=0 2>/dev/null || echo "unknown")
-export COMMIT=$(git rev-parse --short HEAD 2>/dev/null || echo "unknown")
-export DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
+export VERSION=${VERSION:-$(git describe --tags --abbrev=0 2>/dev/null || echo "unknown")}
+export COMMIT=${COMMIT:-$(git rev-parse --short HEAD 2>/dev/null || echo "unknown")}
+export DATE=${DATE:-$(date -u +'%Y-%m-%dT%H:%M:%SZ')}
 
 export LDFLAGS_BASE="-X \"$MODULE/internal/version.Version=$VERSION\" -X \"$MODULE/internal/version.Commit=$COMMIT\" -X \"$MODULE/internal/version.Date=$DATE\""
 export LDFLAGS_RELEASE="-s -w -X \"$MODULE/internal/version.Version=$VERSION\" -X \"$MODULE/internal/version.Commit=$COMMIT\" -X \"$MODULE/internal/version.Date=$DATE\""
